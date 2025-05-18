9b085e0abb9c788f567fb891038a701b000ed15d
HP5000SERIES\avila <avilameza@gmail.com>
2025-05-17 19:36:00 -0500

    4. Rutas Padre y Temple HTML

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'reactive',
+    loadChildren:()=> import('./reactive/reactive.routes').then((modulo)=>modulo.reactiveRoutes),
+  },
+  {
+    path:'auth',
+    loadChildren:()=> import('./auth/auth.routes'),
+  },
+  {
+    path:'country',
+    loadChildren:()=> import('./country/county.routes').then((modulo)=>modulo.countryRoutes),
+  },
+  {
+    path:'**',
+    redirectTo:'reactive'
+  }
+];
--- src/app/shared/components/side-menu/side-menu.component.ts
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { reactiveRoutes } from '../../../reactive/reactive.routes';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface menuItem {
+  title:string;
+  route:string;
+}
+
+const reactiveItems = reactiveRoutes[0].children ?? []
 
 @Component({
   selector: 'app-side-menu',
-  imports: [],
+  imports: [RouterLink,RouterLinkActive],
   templateUrl: './side-menu.component.html',
 })
 export class SideMenuComponent {
+
+  reactiveMenu: menuItem[] = reactiveItems
+  .filter((item) => item.path != '**')
+  .map(item =>({
+    route: `reactive/${item.path}`,
+    title: `${item.title}`,
+  }));
+
+  autMenu: menuItem[] = [
+    {
+      title:'Registro',
+      route:'./auth'
+    }
+  ];
+
+    countyMenu: menuItem[] = [
+    {
+      title:'Paises',
+      route:'./country'
+    }
+  ];
 
 }
