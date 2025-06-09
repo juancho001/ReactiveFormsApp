b6fa4c649d7cc753b402b56aeec1074c2097f024
HP5000SERIES\avila <avilameza@gmail.com>
2025-06-08 21:12:48 -0500

    9. Formularios Dinamicos con Arreglos

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,11 +1,27 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './dynamic-page.component.html',
 })
 export class DynamicPageComponent {
+
+  private formBuilder = inject(FormBuilder)
+
+  myFormDinamic: FormGroup = this.formBuilder.group({
+    name:['',[Validators.required,Validators.minLength(3)]],
+    favoriteGames:this.formBuilder.array([
+      ['Metal Gear',Validators.required],
+      ['Death Stranding', Validators.required]
+    ],Validators.minLength(2)),
+
+  });
+
+  get favoriteGames(){
+    return this.myFormDinamic.get('favoriteGames') as FormArray;
+  }
 
 }
